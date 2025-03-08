fa02504f2466a2faf17c916c3088f6700b96b15d
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-03-08 15:21:11 +0600

    incomplete google map

--- simply-gogle-map/src/main.jsx
+++ simply-gogle-map/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { APIProvider } from '@vis.gl/react-google-maps'
+import { providerKey } from './api-provider-key.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <APIProvider apiKey={providerKey}>
+      <App />
+    </APIProvider>
   </StrictMode>,
 )
